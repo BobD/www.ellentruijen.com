a218a5154c8ae0f2d60817a70680c85ce203c73b
Bob Donderwinkel <b.donderwinkel@bdcreations.nl>
2015-12-07 15:37:43 +0100

    finished the carousel en added some responsive tweaks for the footer, header and grid

--- src/desktop/scripts/main.js
+++ src/desktop/scripts/main.js
@@ -1,34 +1,42 @@
 requirejs.config({
-   paths: {
-   	 jquery: 'vendors/jquery/dist/jquery.min',
-     underscore: 'vendors/underscore/underscore-min',
-     fastclick: 'vendors/fastclick/lib/fastclick',
-     slick: 'vendors/slick-carousel/slick/slick.min',
-   	 domready: 'modules/helpers/domready',
-   	 text: 'modules/helpers/text',
-  }
+    paths: {
+   	    jquery: 'vendors/jquery/dist/jquery.min',
+        underscore: 'vendors/underscore/underscore-min',
+        fastclick: 'vendors/fastclick/lib/fastclick',
+        slick: 'vendors/slick-carousel/slick/slick.min',
+   	    domready: 'modules/helpers/domready',
+   	    text: 'modules/helpers/text',
+    }
 });
 
 require([
 	'!domready',
 	'jquery',
-  'fastclick',
-  'slick'
+    'fastclick',
+    'slick'
 	], function(domReady, $, FastClick) {
 		$('html').removeClass('no-js').addClass('js');
-      FastClick.attach(document.body);
+        FastClick.attach(document.body);
 
-      if($('.carrousel__list').slick){
-        $('.carrousel').addClass('carrousel--slicked');
-        $('.carrousel').removeClass('carrousel--default');
+        if($('.carrousel__list').slick){
+            $('.carrousel').addClass('carrousel--slicked');
+            $('.carrousel').removeClass('carrousel--default');
 
-        $('.carrousel__list').slick({
-          autoplay: true,
-          speed: 400,
-          pauseOnHover: true,
-          easing: 'swing'
-        });
-      }
+            $('.carrousel__list').slick({
+                // autoplay: true,
+                speed: 400,
+                pauseOnHover: true,
+                easing: 'swing'
+            });
+
+            $('.carrousel__list').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+                var infoText = $('*[data-slick-index="' + nextSlide + '"]').find('.carrousel__info').html();
+                $('.carrousel__item-description').html(infoText);
+            });
+
+            var infoText = $('*[data-slick-index="0"]').find('.carrousel__info').html();
+            $('.carrousel__item-description').html(infoText);
+        }
 
 	}
 );
